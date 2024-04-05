be62982af30a3794894da770ed41f7624a0dac07
Sanjai Kannan G <sanjaikannang@gmail.com>
2024-04-05 12:28:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Var in JavaScript :
+
+// function example() {
+//     var x = 10;
+//     if (true) {
+//         var y = 20;
+//         console.log(x); // 10
+//     }
+//     console.log(y); // 20
+// }
+// example();
+
+// OutPut :
+// 10
+// 20 
+
+// ----------------------------------------------------------------
+
+// Let in JavaScript :
+
+// function example() {
+//     let x = 10;
+//     if (true) {
+//         let y = 20;
+//         console.log(x); // 10
+//     }
+//     console.log(y); // ReferenceError: y is not defined
+// }
+// example();
+
+// OutPut :
+// 10
+// ReferenceError: y is not defined
+
+// ----------------------------------------------------------------
+
+// Const in JavaScript :
+
+// function example (){
+//     const x = 10;
+//     if(true){
+//         const y = 20;
+//         console.log(x); // 10
+//     }
+//     console.log(y); // ReferenceError: y is not defined
+// }
+// example()
+
+// OutPut :
+// 10
+// ReferenceError: y is not defined
+
+
+
+
+
+
